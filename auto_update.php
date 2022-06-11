610e34ed1c5d7c9bbe36f3e1eb5322b1b65d7f8c
Kaansi Bewa <81228260+hedhehd@users.noreply.github.com>
2022-06-11 11:52:45 +0200

    Add files via upload

---
+++ auto_update.php
@@ -0,0 +1,7 @@
+19267584|### Changelog 1.26 ###
+- Added QuickLauncher v1.3 (QuickMenuReborn) by Ibrahim778
+- Added CapUnlocker v1.4 by GrapheneCt
+- Added PasteUnlock v1.0 by GrapheneCt
+- Added Repatch reLoaded v1.2 Hotfix by SonicMastr
+- Updated VitaBright v2.0 by devnoname120
+
